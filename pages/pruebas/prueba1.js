70ceb442a247a3569416316f38bbaa7bec05bb9d
Aggge19 <angelagcre19@gmail.com>
2021-11-22 16:47:17 +0100

    base

---
+++ pages/pruebas/prueba1.js
@@ -0,0 +1,14 @@
+function prueba() {
+
+    return (
+        <>
+            <div className="text-center text-6xl bg-yellow-200">
+                <p>Viniciuldinho o melhor jogador do futebol do mundo da història</p>
+                <img className="mx-auto" src="https://imagenes.20minutos.es/files/article_amp/uploads/imagenes/2021/04/07/vinicius-ronaldinho.jpeg"></img>
+                <p>Caralho</p>
+            </div>
+        </>
+    )
+}
+
+export default prueba;
